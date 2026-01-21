7f415e93c2cfdd19708473f72698c520d643771c
József Tar <szurilo@gmail.com>
2026-01-21 17:19:18 +0100

    feat: redirect fix

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -13,7 +13,16 @@ export async function GET(request: Request) {
         const { error } = await supabase.auth.exchangeCodeForSession(code)
 
         if (!error) {
-            return NextResponse.redirect(`${origin}${next}`)
+            const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+            const isLocalEnv = process.env.NODE_ENV === 'development'
+            if (isLocalEnv) {
+                // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+                return NextResponse.redirect(`${origin}${next}`)
+            } else if (forwardedHost) {
+                return NextResponse.redirect(`https://${forwardedHost}${next}`)
+            } else {
+                return NextResponse.redirect(`${origin}${next}`)
+            }
         }
 
         console.error('Error exchanging code for session:', error)
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -18,10 +18,11 @@ export async function createClient() {
                         cookiesToSet.forEach(({ name, value, options }) =>
                             cookieStore.set(name, value, options)
                         )
-                    } catch {
+                    } catch (error) {
                         // The `setAll` method was called from a Server Component.
                         // This can be ignored if you have middleware refreshing
                         // user sessions.
+                        console.error('Error setting cookies in createClient:', error)
                     }
                 },
             },
