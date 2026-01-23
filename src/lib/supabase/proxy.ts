8a19a6d60dab16d88f21ca3a6758bd40f5dabbe5
József Tar <szurilo@gmail.com>
2026-01-23 15:47:23 +0100

    feat: build fix

--- src/lib/supabase/proxy.ts
+++ src/lib/supabase/proxy.ts
@@ -44,7 +44,7 @@ export async function updateSession(request: NextRequest) {
         request.nextUrl.pathname !== "/" &&
         !user &&
         !request.nextUrl.pathname.startsWith("/login") &&
-        !request.nextUrl.pathname.startsWith("/auth") &&
+        !request.nextUrl.pathname.startsWith("/auth")
         // !request.nextUrl.pathname.includes('widget.js') &&
         // !request.nextUrl.pathname.includes('manifest') &&
         // !request.nextUrl.pathname.startsWith('/privacy-policy') &&
